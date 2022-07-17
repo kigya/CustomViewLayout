64811f70994d9f555190887ff06c16a2c99fdd64
kirillborichevskiy <kirilborichevskiy@gmail.com>
2022-07-17 15:47:23 +0300

    BottomButtonsView modification

--- app/src/main/java/com/kigya/customview/BottomButtonsView.kt
+++ app/src/main/java/com/kigya/customview/BottomButtonsView.kt
@@ -3,10 +3,18 @@ package com.kigya.customview
 import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.kigya.customview.databinding.PartButtonsBinding
+
+enum class BottomButtonAction {
+    POSITIVE, NEGATIVE
+}
+
+typealias OnBottomButtonActionListener = (BottomButtonAction) -> Unit
 
 class BottomButtonsView(
     context: Context,
@@ -16,6 +24,7 @@ class BottomButtonsView(
 ) : ConstraintLayout(context, attrs, defStyleAttr, defStyleRes) {
 
     private val binding: PartButtonsBinding
+    private var listener: OnBottomButtonActionListener? = null
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(
         context,
@@ -37,6 +46,7 @@ class BottomButtonsView(
         inflater.inflate(R.layout.part_buttons, this, true)
         binding = PartButtonsBinding.bind(this)
         initAttrs(attrs, defStyleAttr, defStyleRes)
+        initListeners()
     }
 
     private fun initAttrs(attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) {
@@ -80,5 +90,68 @@ class BottomButtonsView(
         }
 
         typedArray.recycle()
+    }
+
+    private fun initListeners() {
+        binding.positiveButton.setOnClickListener {
+            this.listener?.invoke(BottomButtonAction.POSITIVE)
+        }
+        binding.negativeButton.setOnClickListener {
+            this.listener?.invoke(BottomButtonAction.NEGATIVE)
+        }
+    }
+
+    fun setListener(listener: OnBottomButtonActionListener?) {
+        this.listener = listener
+    }
+
+    fun setPositiveButtonText(text: String?) {
+        binding.positiveButton.text = text ?: "Ok"
+    }
+
+    fun setNegativeButtonText(text: String?) {
+        binding.negativeButton.text = text ?: "Cancel"
+    }
+
+    override fun onSaveInstanceState(): Parcelable {
+        val superState = super.onSaveInstanceState()!!
+        val savedState = SavedState(superState)
+        savedState.positiveButtonText = binding.positiveButton.text.toString()
+        return savedState
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        val savedState = state as SavedState
+        super.onRestoreInstanceState(savedState.superState)
+        binding.positiveButton.text = savedState.positiveButtonText
+    }
+
+    class SavedState : BaseSavedState {
+
+        var positiveButtonText: String? = null
+
+        constructor(superState: Parcelable) : super(superState)
+
+        constructor(parcel: Parcel) : super(parcel) {
+            positiveButtonText = parcel.readString()
+        }
+
+        override fun writeToParcel(out: Parcel, flags: Int) {
+            super.writeToParcel(out, flags)
+            out.writeString(positiveButtonText)
+        }
+
+        companion object {
+            @JvmField
+            val CREATOR: Parcelable.Creator<SavedState> = object : Parcelable.Creator<SavedState> {
+                override fun createFromParcel(source: Parcel): SavedState {
+                    return SavedState(source)
+                }
+
+                override fun newArray(size: Int): Array<SavedState?> {
+                    return Array(size) { null }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/kigya/customview/MainActivity.kt
+++ app/src/main/java/com/kigya/customview/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.kigya.customview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.kigya.customview.databinding.ActivityMainBinding
-import com.kigya.customview.databinding.PartButtonsBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +14,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.bottomButtons.setListener {
+            if (it == BottomButtonAction.POSITIVE) {
+                binding.bottomButtons.setPositiveButtonText("Updated OK")
+                Toast.makeText(this, "Positive Button Pressed", Toast.LENGTH_SHORT).show()
+
+            } else if (it == BottomButtonAction.NEGATIVE) {
+                binding.bottomButtons.setNegativeButtonText("Updated Cancel")
+                Toast.makeText(this, "Negative Button Pressed", Toast.LENGTH_SHORT).show()
+
+            }
+        }
     }
 }
